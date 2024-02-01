4786f25fb0a4443625d4067db550b037e0591e30
ttv-eu c1 <ttv-eu-cohort-1@ephemerecreative.ca>
2024-02-01 16:16:38 +0000

    fix linting and ts issues:

--- src/modules/messages/hooks/use-message-by-id/use-message-by-id.ts
+++ src/modules/messages/hooks/use-message-by-id/use-message-by-id.ts
@@ -1,13 +1,13 @@
 import useSWR from "swr";
-import { getAllMessages } from "../../lib/get-all-messages/get-all-messages";
 import { useCallback } from "react";
+import { getMessageById } from "../../lib/get-messages-by-id/get-messages-by-id";
 
-export function useMessageById() {
+export function useMessageById(id: string) {
   const fetcher = useCallback(
     async ([path, innerId]: [path: string, id: string]) => {
       return await getMessageById(innerId);
     },
     [],
   );
-  return useSWR(["/api/Messages", id], fetcher);
+  return useSWR(["/api/messages", id], fetcher);
 }

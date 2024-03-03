f32d335e90b2659cd05ba7fae658c7cd82cea57e
Clinton John <cjxfs2007@gmail.com>
2024-03-03 17:34:07 +0000

    fixed apostrophe issue

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,20 @@
+export default function About_Us() {
+  return (
+    <body className="about-us">
+      <div>
+        <div>
+          <nav></nav>
+        </div>
+        <div>
+          <main>
+            <p>
+              Well, you don&apos;t need to know about us, but we&apos;ll tell
+              you anyways. It all started with a grand idea. The idea came from
+              a guy called Clinton. He came up with this idea because....
+            </p>
+          </main>
+        </div>
+      </div>
+    </body>
+  );
+}

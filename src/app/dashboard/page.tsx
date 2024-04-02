a08a2663b77fd0c0f4555194a38b7529c1074559
Clinton John <cjxfs2007@gmail.com>
2024-04-02 23:46:40 +0000

    minor corrections II

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -43,6 +43,7 @@ export default function HomePage() {
             backgroundColor: "white",
             borderRadius: 50,
             justifyContent: "center",
+            overflowY: "scroll",
           }}
           minWidth={600}
           height={500}

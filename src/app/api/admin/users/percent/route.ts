36bec031f26955063ef930ffe948d0ef3fc7253f
Harrison John <harrisjohnu@gmail.com>
2024-10-12 13:10:50 +0000

    fix(CAUK-3009): added first invocation

--- src/app/api/admin/users/percent/route.ts
+++ src/app/api/admin/users/percent/route.ts
@@ -6,8 +6,8 @@ export const GET = async () => {
   const past30Days = new Date(now.setDate(now.getDate() - 30));
 
   // Query for users who logged in within the last 30 days
-  const activeUsers = 100;
-  const totalUsers = 7;
+  const activeUsers = 1;
+  const totalUsers = await prisma.user.count();
   const activeUsersCount = ((activeUsers / totalUsers) * 100).toFixed(1); // Convert to percentage
   console.log(activeUsersCount);
   return NextResponse.json(activeUsersCount);

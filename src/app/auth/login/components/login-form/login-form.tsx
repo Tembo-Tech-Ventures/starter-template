b88aeb9c4b42f0c23bb67b83fcb55839901c9038
Harrison John <harrisjohnu@gmail.com>
2025-01-17 18:50:50 +0000

    chore(CAUK-3009): updated node, npm and prisma

--- src/app/auth/login/components/login-form/login-form.tsx
+++ src/app/auth/login/components/login-form/login-form.tsx
@@ -25,183 +25,64 @@ export function LoginForm() {
     fontFamily: "'Indie Flower', cursive",
   };
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <Stack
-        style={{
-          backgroundImage: "url('/Login.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+        sx={{
+          display: "flex",
+          position: "relative",
+          width: "100%",
+          height: "100%",
           alignItems: "center",
           justifyContent: "center",
-          display: "flex",
-          cursor: "none",
         }}
-        onMouseOver={(e) => {
-          var mouse = document.getElementById("mouse") as HTMLElement;
-          var pointer = document.getElementById("pointer") as HTMLElement;
-          console.log(mouse);
-          window.addEventListener("mousemove", (t) => {
-            mouse!.style.top = `${t.clientY}px`;
-            mouse!.style.left = `${t.clientX}px`;
-            pointer!.style.top = `${t.clientY}px`;
-            pointer!.style.left = `${t.clientX}px`;
-          });
-        }}
-        width="100%"
-        minHeight="100vh"
-        display={"flex"}
       >
-        <Image
-          src={"/cursor.png"}
-          alt="cursor"
-          id="mouse"
-          width={30}
-          height={30}
-          style={{
-            display: "block",
-            zIndex: 9999,
-            position: "absolute",
-            pointerEvents: "none",
-          }}
-          onClick={(l) => {
-            return true;
-          }}
-        ></Image>
-        <Image
-          src={"/pointer.png"}
-          alt="cursor"
-          id="pointer"
-          width={20}
-          height={30}
-          style={{
-            display: "none",
-            zIndex: 9999,
-            position: "absolute",
-            pointerEvents: "none",
-          }}
-          onClick={(l) => {
-            return true;
-          }}
-        ></Image>
         <Stack
-          style={{
-            backgroundColor: "rgba(255, 255, 255, 0.3)",
-            width: 410,
+          sx={{
+            backgroundColor: "rgba(255,0,0,0.4)",
+            width: { xs: 300, sm: 345, md: 412, lg: 450, xl: 500 },
+            borderRadius: 9,
             alignItems: "center",
             justifyContent: "center",
           }}
+          spacing={2}
         >
-          <form
-            onSubmit={(p) => {
-              p.preventDefault();
-              signIn("email", { email: (p.target as any).email.value });
-              setSubmitted(true);
+          <Typography
+            variant="h4"
+            sx={{
+              display: "flex",
+              position: "relative",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              fontFamily: "fantasy",
+              fontSize: { xs: 26, sm: 31, md: 35, lg: 38, xl: 42 },
             }}
           >
-            <Stack
-              direction={"column"}
-              style={{ height: "30vh" }}
-              spacing={5}
-              borderTop={90}
-              borderColor={"transparent"}
-            >
-              <Typography
-                fontSize={22}
-                style={{ fontFamily: "'Indie Flower', cursive" }}
-                fontStyle={"normal"}
-                fontWeight={400}
-                lineHeight="normal"
-                onMouseOver={(r) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  cursor.srcset = "/text-cursor.png";
-                }}
-                onMouseOut={(i) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  cursor.srcset = "/cursor.png";
-                }}
-              >
-                Sign into your account
-              </Typography>
-            </Stack>
-            <Stack
-              spacing={2}
-              style={{ width: 220, height: "30vh" }}
-              borderBottom={50}
-              borderColor={"transparent"}
-            >
-              <TextField
-                name="email"
-                id="email"
-                label="Email"
-                inputProps={{ style: inputStyle }}
-                style={{
-                  width: 220,
-                  flexDirection: "column",
-                  borderRadius: 50,
-                }}
-                value={email}
-                onChange={(o) => setEmail(o.target.value)}
-                onMouseOver={(r) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  var email = document.getElementById(
-                    "email",
-                  ) as HTMLInputElement;
-                  email.style.cursor = "none";
-                  cursor.srcset = "/text-cursor.png";
-                }}
-                onMouseOut={(i) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  cursor.srcset = "/cursor.png";
-                }}
-                required
-              ></TextField>
-            </Stack>
-            <Stack
-              borderTop={10}
-              borderColor={"transparent"}
-              direction={"column"}
-              display={"flex"}
-              style={{ width: 220, position: "relative", bottom: 50 }}
-            >
-              <Button
-                variant="contained"
-                type="submit"
-                style={{
-                  fontFamily: "Train_One",
-                  width: 220,
-                  borderRadius: 50,
-                  backgroundColor: "purple",
-                  cursor: "none",
-                }}
-                onMouseOver={(r) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  cursor.srcset = "/pointer.png";
-                  cursor.height = 30;
-                  cursor.width = 20;
-                }}
-                onMouseOut={(i) => {
-                  var cursor = document.getElementById(
-                    "mouse",
-                  ) as HTMLImageElement;
-                  cursor.srcset = "/cursor.png";
-                  cursor.height = 30;
-                  cursor.width = 30;
-                }}
-              >
-                <span>Login</span>
-              </Button>
-            </Stack>
-          </form>
+            Login
+          </Typography>
+          <TextField
+            label="Email"
+            type="email"
+            value={email}
+            sx={{ width: "70%" }}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            value={"In Progress"}
+            type="password"
+            label="Password"
+            sx={{ width: "70%" }}
+          />
+          <Button variant="contained">Login</Button>
         </Stack>
       </Stack>
     </Box>
